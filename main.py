8b46c8c09928d2849362b90feff3df01e6eaa42c
Wojciech Dranka <dr.woj@wp.pl>
2024-01-16 00:58:31 +0100

    Scrape position names using class name - change url for the one with applied filters

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import httpx
 from selectolax.parser import HTMLParser
 
 url = "https://justjoin.it/"
+url2 = "https://justjoin.it/all-locations/data/experience-level_junior/remote_yes"
+
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                          "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
 
-response = httpx.get(url, headers=headers)
+response = httpx.get(url2, headers=headers)
+html = HTMLParser(response.text)
+jobs = html.css("div.css-2crog7")
 
-print(response)
+for job in jobs:
+    print(job.css_first("h2.css-16gpjqw").text())
+
+
